e309302f6f2a5bd5614d91559c00706f50d4d878
cjerry80 <cjerry80@cs3006b.acad.fullerton.edu>
2019-03-14 16:44:32 -0700

    Getters and setters

--- person.cpp
+++ person.cpp
@@ -4,39 +4,72 @@
 using namespace std;
 
 
-
 Person::Person(){
-string firstn;
-string lastn;
-float taxR;
-float hoursW;
+firstName = " ";
+lastName = " ";
+employeeID = 0;
+companyName = " ";
+hoursWorked = 0.0;
+payRate = 0.0;
 
 }
-string Person::getLastName(){
-return lastName;
-}
+
 void Person::setFirstName(string fName){
-firstn = fName;
+firstName = fname;
 }
+
 string Person::getFirstName(){
 return firstName;
 }
-void Person::setPayRate(float rate){
-taxR = rate;
+
+void Person::setLastName(string lName){
+lastName = lName;
 }
+
+string Person::getLastName(){
+return lastName;
+}
+
+void Person::setEmployeeId(int id){
+employeeID = id;
+}
+
+int Person::getEmployeeId(){
+return employeeID;
+}
+
+void Person::setCompanyName(string coName){
+companyName = coName;
+}
+
+string Person::getCompanyName(){
+return companyName;
+}
+
+void Person::setPayRate(float rate){
+payRate = rate;
+}
+
 float Person::getPayRate(){
 return payRate;
+}
 
-}
 void Person::setHoursWorked(float hours){
-hoursW = hours;
+hoursWorked = hours;
 }
+
 float Person::getHoursWorked(){
 return hoursWorked;
 }
+
 float Person::totalPay(){
-
+float totalPay;
+totalPay = hoursWorked * payRate;
+return totalPay;
 }
-string Person::fullName(){
 
+string Person::fullName(){
+string fullname;
+fullname = firstName + " " + lastName;
+return fullname;
 }
